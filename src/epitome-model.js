57765c64163bcc4a418ebc0db117e4f7e2300991
DimitarChristoff <christoff@gmail.com>
2012-06-12 14:57:25 +0100

    attributes added now

--- src/epitome-model.js
+++ src/epitome-model.js
@@ -8,10 +8,6 @@
 
         _attributes: {},
 
-        // custom accessors.
-        properties: {},
-
-        // initial `private` object
         options: {
             defaults: {}
         },
@@ -20,57 +16,27 @@
             // constructor for Model class.
 
             // initial obj should pass on a setter (this will fail for now).
-            obj && typeOf(obj) === 'object' && this.set(Object.merge(this.options.defaults, obj));
+            obj && typeOf(obj) === 'object' && this.set(obj);
 
             // merge options overload, will now add the events.
             this.setOptions(options);
         },
 
-        set: function() {
-            // call the real getter. we proxy this because we want
-            // a single event after all properties are updated and the ability to work with
-            // either a single key, value pair or an object
-            this.propertiesChanged = [];
-            this._set.apply(this, arguments);
-            // if any properties did change, fire a change event with the array.
-            this.propertiesChanged.length && this.fireEvent('change', this.propertiesChanged);
-        },
-
         // private, real setter functions, not on prototype, see note above
-        _set: function(key, value) {
+        set: function(key, value) {
             // needs to be bound the the instance.
             if (!key || typeof value === undefined) return this;
 
-            // custom setter - see bit further down
-            if (this.properties[key] && this.properties[key]['set'])
-                return this.properties[key]['set'].call(this, value);
-
-
-            // no change? this is crude and works for primitives.
-            if (this._attributes[key] && this._attributes[key] === value)
-                return this;
-
-            if (value === null) {
-                delete this._attributes[key]; // delete = null.
-            }
-            else {
-                this._attributes[key] = value;
-            }
-
-            // fire an event.
-            this.fireEvent('change:' + key, value);
-
-            // store changed keys...
-            this.propertiesChanged.push(key);
+            this._attributes[key] = value;
 
             return this;
-        }.overloadSetter(),   // mootools abstracts overloading to allow object iteration
+        },
 
         get: function(key) {
-            // and the overload getter
             return (key && typeof this._attributes[key] !== undefined)
-                ? this.properties[key] && this.properties[key]['get'] ? this.properties[key]['get'].call(this) : this._attributes[key] : null;
-        }.overloadGetter(),
+                ? this._attributes[key]
+                : null;
+        },
 
         toJSON: function() {
             return Object.clone(this._attributes);
